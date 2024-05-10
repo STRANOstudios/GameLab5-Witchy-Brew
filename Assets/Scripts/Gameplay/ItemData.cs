d7bb05ad1c44866568ddce54d2aff64b34af9413
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-05-10 17:46:43 +0200

    test scene

---
+++ Assets/Scripts/Gameplay/ItemData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+[CreateAssetMenu]
+public class ItemData : ScriptableObject
+{
+
+    [SerializeField] Sprite image;
+    [SerializeField] string name;
+    [SerializeField] int id;
+
+
+
+
+}
