39d57fc86964f2a2530a872d70b923002550ffdb
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-05-21 00:20:05 +0200

    start potion implementation

--- Assets/Scripts/Gameplay/Potion Generator.cs
+++ Assets/Scripts/Gameplay/Potion Generator.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class PotionGenerator : MonoBehaviour
 {
     CraftedIngredient[] ingredients=new CraftedIngredient[5];
-    [SerializeField]List<ItemData> itemData = new List<ItemData>();
+    [SerializeField]List<ItemData> itemDatas = new List<ItemData>();
+    [SerializeField] List<Preparation> preparations = new List<Preparation>();
+    List<ItemData> ingredientList;
 
-    private void Start()
+    private void GeneratePotion()
     {
+        ingredientList = itemDatas;
         for(int i = 0; i < ingredients.Length; i++)
         {
-            ingredients[i] = new CraftedIngredient();
+            CraftedIngredient slot=new CraftedIngredient();
+            int random = Random.Range(0, 12);
+            slot.itemData = ingredientList[Random.Range(0,12)];
+
+            slot.preparation = preparations[Random.Range(0,4)];
+            ingredients[i] = slot;
         }
+
     }
-
-
+    private void Start()
+    {
+        GeneratePotion();
+    }
 
 
 
