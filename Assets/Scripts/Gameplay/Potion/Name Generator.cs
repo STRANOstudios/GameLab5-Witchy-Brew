985f84626d06924eee2471515fd4269f7bfeb929
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-05-25 23:31:01 +0200

    Random COlor generation

--- Assets/Scripts/Gameplay/Potion/Name Generator.cs
+++ Assets/Scripts/Gameplay/Potion/Name Generator.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NameGenerator : MonoBehaviour
 {
 
-
+    [SerializeField]Image image;
     public static NameGenerator instance;
     [SerializeField]List<string> color = new List<string>();
+    [SerializeField]public List<Color> potionColor = new List<Color>();
     [SerializeField]List<string> type = new List<string>();
     [SerializeField]List<string> adjective = new List<string>();
     [SerializeField]List<string> goal = new List<string>();
@@ -21,6 +23,7 @@ public class NameGenerator : MonoBehaviour
     {
         string potionName = "";
         int randomColor=Random.Range(0, color.Count);
+        image.color=potionColor[randomColor];
         int randomType = Random.Range(0, type.Count);
         int randomAdjective = Random.Range(0, adjective.Count);
         int randomGoal= Random.Range(0, goal.Count);
